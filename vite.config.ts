79634250cb4846486788146791c7d09b9299169c
amarpreetbhatia <amarpreetbhatia@gmail.com>
2025-08-08 15:33:43 +1000

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,19 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/amarpreetbhatia.github.io/' : '/',
+  base: mode === 'production' ? './' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
     },
   },
 })) 
